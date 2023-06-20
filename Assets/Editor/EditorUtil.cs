eab1a2429fa6682189d4a55f6f47ce38361005ee
xue-fei <awnuxcvbn@163.com>
2023-06-20 16:38:24 +0800

    用Unity做一个Win壁纸程序

---
+++ Assets/Editor/EditorUtil.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class EditorUtil : MonoBehaviour
+{
+    [MenuItem("工具/打开沙盒文件夹")]
+    static void OpenPersistentDataPath()
+    {
+        System.Diagnostics.Process.Start(@Application.persistentDataPath);
+    }
+}
---
+++ Assets/Scripts/Effect1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Effect1 : MonoBehaviour
+{
+    public Material m;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        if (m.color != Color.blue)
+        {
+            m.color = Color.blue;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (m.color != Color.green)
+        {
+            m.color = Color.green;
+        }
+    }
+}
